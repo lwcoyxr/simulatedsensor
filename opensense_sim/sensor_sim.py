97b31370d4a53a04ac65d0df0ce3342fd7d287ff
Stanislav Sliesariev <stanislav.sliesariev@reface.ai>
2025-05-28 11:33:49 +0300

    feat: ms sensor

--- opensense_sim/sensor_sim.py
+++ opensense_sim/sensor_sim.py
@@ -5,7 +5,8 @@ import os
 
 # Environment variables
 BOX_TOKEN = os.getenv("BOX_TOKEN")
-SENSOR_ID = os.getenv("SENSOR_ID")  # This is the sensor's ID, not the token
+SENSOR_ID = os.getenv("SENSOR_ID")
+RAD_SENSOR_ID = os.getenv("")
 INTERVAL = int(os.getenv("INTERVAL", "30"))  # seconds between sends
 
 API_URL = f"https://api.opensensemap.org/boxes/{BOX_TOKEN}/data"
@@ -13,18 +14,27 @@ API_URL = f"https://api.opensensemap.org/boxes/{BOX_TOKEN}/data"
 def generate_value():
     return round(random.uniform(700.0, 3000.0), 2)
 
+def generate_radiation_value():
+    return round(random.uniform(0.30, 999999.0), 3)
+
 def send_data():
     value = generate_value()
+    ms_value = generate_radiation_value()
     payload = [
         {
             "sensor": SENSOR_ID,
             "value": value
         }
+        {
+            "sensor": RAD_SENSOR_ID,
+            "value": ms_value
+        }
     ]
     headers = {"Content-Type": "application/json"}
     response = requests.post(API_URL, json=payload, headers=headers)
-
-    print(f"Sent value {value} - Status code: {response.status_code} - Response: {response.text}")
+    
+    print(f"Status code: {response.status_code} - Response: {response.text}")
+    print(f"Sent values -> Sensor1: {value}, Radiation: {ms_value} µSv/h")
 
 if __name__ == "__main__":
     while True:
